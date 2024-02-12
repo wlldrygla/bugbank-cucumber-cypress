60e3fa101c464c12645f1f05da98b8ad270a3e54
Will Drygla <willian.drygla@icloud.com>
2024-02-12 09:33:43 -0300

    Feat: Remove cucumber and make Work Flows

--- cypress/e2e/register/register.cy.ts
+++ cypress/e2e/register/register.cy.ts
@@ -1,20 +1,21 @@
-import InitialPage from '../../support/pageobjects/initialPage';
-import RegisterPage from '../../support/pageobjects/registerPage';
+import { RegisterPageFlows } from '../../support/workFlows/registerFlows';
 
-const registerPage = new RegisterPage();
-const initialPage = new InitialPage();
+const registerFlows = new RegisterPageFlows();
 
 describe('e2e testing registry on BugBank', () => {
 	beforeEach(() => {
 		cy.visit('https://bugbank.netlify.app');
 	});
-	it('should fill the form correctly and save', () => {
-		initialPage.acessRegisterPage();
-		registerPage.fillEmail('willian.cardoso@gmail.com');
-		registerPage.fillName('Willian Drygla');
-
-		registerPage.fillPassword('Senha123@');
-		registerPage.fillConfirmationPassword('Senha123@');
-		registerPage.submit();
-		cy.get('#modalText').should('contain', 'criada com sucesso');	});
+	it('should create an account with balance and save', () => {
+		registerFlows.acessInitialPage();
+		registerFlows.fillForm({
+			email: 'willian.cardoso@automatedtests.com',
+			name: 'Willian Tests',
+			password: 'Senha123@',
+			confirmPassword: 'Senha123@',
+			balance: true,
+		});
+		registerFlows.submitForm();
+		cy.get('#modalText').should('contain', 'criada com sucesso');
+	});
 });
